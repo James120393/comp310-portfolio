a5d2170dd06706ea4e36ed6005978e2f1b1d0c99
James120393 <james_hellman@hotmail.co.uk>
2017-11-23 23:13:08 +0000

    Basic Game, doesn't look pretty but it works... mostly

--- Game/controller.asm
+++ Game/controller.asm
@@ -17,7 +17,8 @@ timerEnemy          .rs 1 ; Stores a valuse for the enemy spawn Timer
 gravity				.rs 1 ; Stores the value for Gravity
 enemyX				.rs 1 ; Stores the enemies X position
 enemyY				.rs 1 ; Stores the enemies Y position
-randLocation     .rs 1 ; Stores a random number for gem locations
+randLocation        .rs 1 ; Stores a random number for gem locations
+randLocation2       .rs 1 ; Stores a random number for gem locations
 
 
 CONTROLLER_A      = %10000000
@@ -98,7 +99,7 @@ LoadSpritesLoop:
   LDA sprites, x        ; load data from address (sprites +  x)
   STA $0200, x          ; store into RAM address ($0200 + x)
   INX                   ; X = X + 1
-  CPX #$30              ; Compare X to hex $20, decimal 32
+  CPX #$32              ; Compare X to hex $20, decimal 32
   BNE LoadSpritesLoop   ; Branch to LoadSpritesLoop if compare was Not Equal to zero
                         ; if compare was equal to 32, keep going down
               
@@ -121,21 +122,43 @@ NMI:
   LDA #$02
   STA $4014       ; set the high byte (02) of the RAM address, start the transfer
   
+EndGame:
+  LDA $022D             ; Load Tile number
+  CMP #$F9              ; Compare it to 10
+  BNE .Done             ; Branch if not equal
+   
+  JMP Forever           ; End game 
+.Done:
   
-  ; Update fire X position
-  LDA $0213
-  CLC
-  ADC #1
-  STA $0213
-  LDA $0217
-  CLC
-  ADC #1
-  STA $0217
 
+  ; Update fire X position
+  LDA $0213             ; Load Fire X Location
+  CLC                   ; Clear Carry flag
+  ADC #1                ; Add one
+  STA $0213             ; Increment the X location by 1
+  LDA $0217             ; Load Fire X Location
+  CLC					; Clear Carry flag
+  ADC #1				; Add one
+  STA $0217				; Increment the X location by 1
+
+
+RadomNumber:
   LDA randLocation       ; Load randLoaction
   CLC                    ; Clear Carry flag
   ADC #1                 ; Add 1
   STA randLocation       ; Save to randLocation
+  CMP #80               ; Do this 20 times then move on
+  BNE .Done              ; Branch to done
+.Done
+
+RadomNumber1:
+  LDA randLocation2       ; Load randLoaction
+  CLC                    ; Clear Carry flag
+  ADC #1                 ; Add 1
+  STA randLocation2       ; Save to randLocation
+  CMP #80               ; Do this 20 times then move on
+  BNE .Done              ; Branch to done
+.Done
  
 
  ; Used to drag player down 
@@ -178,13 +201,20 @@ MoveEnemies:
   INX
   INX
   INX
-  LDA timerEnemy
-  CLC
-  ADC #1
-  STA timerEnemy
-  CMP #20  
   CPX #$10
   BNE .Loop
+
+.Loop2:
+  LDA $0218, x           ; load sprite Y position
+  SEC                    ; make sure the carry flag is clear
+  SBC #$01               ; A = A + 1
+  STA $0218, x           ; save sprite Y position
+  INX
+  INX
+  INX
+  INX
+  CPX #$10
+  BNE .Loop2
 .Done:
 
 
@@ -232,8 +262,10 @@ UpdateEnemy0:
   SBC #12				 ; Subtract 12
   BPL .Done 			 ; Branch if FireX - EnemyX - 8 > 0
   
-  LDA randLocation				 ; Load the value 0
+  LDA randLocation		 ; Load random value
   STA $0218				 ; Save into the memory locations of the enemy to remove them from screen
+  CLC
+  LDA randLocation2		 ; Load random value
   STA $021B
 
 .Done:
@@ -261,8 +293,10 @@ UpdateEnemy1:
   SBC #12				 ; Subtract 12
   BPL .Done 			 ; Branch if FireX - EnemyX - 8 > 0
   
-  LDA randLocation				 ; Load the value 0
+  LDA randLocation	     ; Load random value
   STA $021C				 ; Save into the memory locations of the enemy to remove them from screen
+  CLC
+  LDA randLocation2		 ; Load random value
   STA $021F
 .Done:
 
@@ -289,8 +323,10 @@ UpdateEnemy2:
   SBC #12				 ; Subtract 12
   BPL .Done 			 ; Branch if FireX - EnemyX - 8 > 0
   
-  LDA randLocation				 ; Load the value 0
+  LDA randLocation		 ; Load random value
   STA $0220				 ; Save into the memory locations of the enemy to remove them from screen
+  CLC
+  LDA randLocation2		 ; Load random value
   STA $0223
 .Done:
 
@@ -317,12 +353,151 @@ UpdateEnemy3:
   SBC #12				 ; Subtract 12
   BPL .Done 			 ; Branch if FireX - EnemyX - 8 > 0
   
-  LDA randLocation				 ; Load the value 0
+  LDA randLocation		 ; Load random value
   STA $0224				 ; Save into the memory locations of the enemy to remove them from screen
+  CLC
+  LDA randLocation2		 ; Load random value
   STA $0227
 .Done:
 
+PlayerCollisionCheck0:
+  ; Check collision
+  LDA $0200              ; Fire Y
+  SEC					 ; Set carry flag
+  SBC $0218 			 ; Enemy Y
+  CLC					 ; Clear Carry flag
+  ADC #8				 ; Add 8
+  BMI .Done 			 ; Branch if FireY - EnemyY + 8 < 0
+  SEC					 ; Set carry flag
+  SBC #12				 ; Subtract 12
+  BPL .Done 			 ; Branch if FireY - EnemyY - 8 > 0
+  
+  LDA $0203 			 ; Fire X
+  SEC					 ; Set carry flag
+  SBC $021B 			 ; Enemy X
+  CLC					 ; Clear Carry flag
+  ADC #8				 ; Add 8
+  BMI .Done 			 ; Branch if FireX - EnemyX + 8 < 0
+  SEC					 ; Set carry flag
+  SBC #12				 ; Subtract 12
+  BPL .Done 			 ; Branch if FireX - EnemyX - 8 > 0
+  
+  LDA randLocation		 ; Load random value
+  STA $0218				 ; Save into the memory locations of the enemy to remove them from screen
+  CLC
+  LDA randLocation2		 ; Load random value
+  STA $021B
 
+  LDA $022D              ; Load Number Tile
+  CLC					 ; Clear Carry Flag
+  ADC #9				 ; Add one
+  STA $022D				 : Incremnt the Score by 1
+.Done:
+
+
+PlayerCollisionCheck1:
+  ; Check collision
+  LDA $0200              ; Fire Y
+  SEC					 ; Set carry flag
+  SBC $021C 			 ; Enemy Y
+  CLC					 ; Clear Carry flag
+  ADC #8				 ; Add 8
+  BMI .Done 			 ; Branch if FireY - EnemyY + 8 < 0
+  SEC					 ; Set carry flag
+  SBC #12				 ; Subtract 12
+  BPL .Done 			 ; Branch if FireY - EnemyY - 8 > 0
+  
+  LDA $0203 			 ; Fire X
+  SEC					 ; Set carry flag
+  SBC $021F 			 ; Enemy X
+  CLC					 ; Clear Carry flag
+  ADC #8				 ; Add 8
+  BMI .Done 			 ; Branch if FireX - EnemyX + 8 < 0
+  SEC					 ; Set carry flag
+  SBC #12				 ; Subtract 12
+  BPL .Done 			 ; Branch if FireX - EnemyX - 8 > 0
+  
+  LDA randLocation	     ; Load random value
+  STA $021C				 ; Save into the memory locations of the enemy to remove them from screen
+  CLC
+  LDA randLocation2		 ; Load random value
+  STA $021F
+
+  LDA $022D              ; Load Number Tile
+  CLC					 ; Clear Carry Flag
+  ADC #9				 ; Add one
+  STA $022D				 : Incremnt the Score by 1
+.Done:
+
+
+PlayerCollisionCheck2:
+  ; Check collision
+  LDA $0200              ; Fire Y
+  SEC					 ; Set carry flag
+  SBC $0220 			 ; Enemy Y
+  CLC					 ; Clear Carry flag
+  ADC #8				 ; Add 8
+  BMI .Done 			 ; Branch if FireY - EnemyY + 8 < 0
+  SEC					 ; Set carry flag
+  SBC #12				 ; Subtract 12
+  BPL .Done 			 ; Branch if FireY - EnemyY - 8 > 0
+  
+  LDA $0203 			 ; Fire X
+  SEC					 ; Set carry flag
+  SBC $0223 			 ; Enemy X
+  CLC					 ; Clear Carry flag
+  ADC #8				 ; Add 8
+  BMI .Done 			 ; Branch if FireX - EnemyX + 8 < 0
+  SEC					 ; Set carry flag
+  SBC #12				 ; Subtract 12
+  BPL .Done 			 ; Branch if FireX - EnemyX - 8 > 0
+  
+  LDA randLocation		 ; Load random value
+  STA $0220				 ; Save into the memory locations of the enemy to remove them from screen
+  CLC
+  LDA randLocation2		 ; Load random value
+  STA $0223
+
+  LDA $022D              ; Load Number Tile
+  CLC					 ; Clear Carry Flag
+  ADC #9				 ; Add one
+  STA $022D				 : Incremnt the Score by 1
+.Done:
+
+
+PlayerCollisionCheck3:
+  ; Check collision
+  LDA $0200              ; Fire Y
+  SEC					 ; Set carry flag
+  SBC $0224 			 ; Enemy Y
+  CLC					 ; Clear Carry flag
+  ADC #8				 ; Add 8
+  BMI .Done 			 ; Branch if FireY - EnemyY + 8 < 0
+  SEC					 ; Set carry flag
+  SBC #12				 ; Subtract 12
+  BPL .Done 			 ; Branch if FireY - EnemyY - 8 > 0
+  
+  LDA $0203 			 ; Fire X
+  SEC					 ; Set carry flag
+  SBC $0227 			 ; Enemy X
+  CLC					 ; Clear Carry flag
+  ADC #8				 ; Add 8
+  BMI .Done 			 ; Branch if FireX - EnemyX + 8 < 0
+  SEC					 ; Set carry flag
+  SBC #12				 ; Subtract 12
+  BPL .Done 			 ; Branch if FireX - EnemyX - 8 > 0
+  
+  LDA randLocation		 ; Load random value
+  STA $0224				 ; Save into the memory locations of the enemy to remove them from screen
+  CLC
+  LDA randLocation2		 ; Load random value
+  STA $0227
+
+  LDA $022D              ; Load Number Tile
+  CLC					 ; Clear Carry Flag
+  ADC #9				 ; Add one
+  STA $022D				 : Incremnt the Score by 1
+.Done:
 
 UpdateGem:
   ; Check collision
@@ -346,11 +521,19 @@ UpdateGem:
   SBC #12				 ; Subtract 12
   BPL .Done              ; Branch if PlayerX - GemX - 8 > 0
   
-  LDA randLocation                ; Load the value 0
+  LDA randLocation       ; Load random value
   STA $0228              ; Save into the memory locations of the gem to remove them from screen
+  CLC
+  LDA randLocation2		 ; Load random value
   STA $022B
+
+  LDA $022D              ; Load Number Tile
+  CLC                    ; Clear Carry Flag
+  ADC #1                 ; Add one
+  STA $022D              : Incremnt the Score by 1
  
 .Done:
+
 
   JSR ReadController1
 
@@ -360,7 +543,7 @@ ReadLeft:
   AND #CONTROLLER_LEFT   ; only look at bit 0
   BEQ .Done              ; branch to ReadADone if button is NOT pressed (0)
 				         
-  LDA $0203              ; Load Sprite Y Position
+  LDA $0203              ; Load Sprite X Position
   CMP #LEFTWALL          ; Compare it to the Bottom wall position
   BEQ .Done              ; Branch to jump if the Y position and wall match
 				         
@@ -502,14 +685,14 @@ sprites:
   .db $80, $01, $00, $88   ;sprite 1
   .db $88, $10, $00, $80   ;sprite 2
   .db $88, $11, $00, $88   ;sprite 3
-  .db $00, $02, $00, $00   ;bullet 0
-  .db $00, $03, $00, $08   ;bullet 1
-  .db $20, $12, $00, $88   ;enemy0
-  .db $80, $12, $00, $88   ;enemy2
-  .db $50, $12, $00, $88   ;enemy3
-  .db $90, $12, $00, $88   ;enemy4
-  .db $40, $30, $00, $88   ;gem
-
+  .db $00, $02, $00, $00   ;Fire 0
+  .db $00, $03, $00, $08   ;Fire 1
+  .db $20, $12, $00, $00   ;enemy0
+  .db $80, $12, $00, $00   ;enemy2
+  .db $50, $12, $00, $00   ;enemy3
+  .db $90, $12, $00, $00   ;enemy4
+  .db $40, $30, $00, $00   ;gem
+  .db $10, $F0, $00, $10   ;One
 
   .org $FFFA     ;first of the three vectors starts here
   .dw NMI        ;when an NMI happens (once per frame if enabled) the 
